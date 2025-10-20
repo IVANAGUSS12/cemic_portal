4db6009c38759c0d25d8f6debee76b0e60979e43
IVANAGUSS12 <ivanaguss12@gmail.com>
2025-10-20 10:38:31 -0300

    Update settings.py

--- autorizaciones/settings.py
+++ autorizaciones/settings.py
@@ -1,16 +1,24 @@
 import os
 from pathlib import Path
+from datetime import timedelta
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-def get_env(name, default=None):
-    return os.getenv(name, default)
+SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
 
-SECRET_KEY = get_env('SECRET_KEY','dev-insecure-key')
-DEBUG = get_env('DEBUG','1') == '1'
+DEBUG = os.getenv("DEBUG", "0") == "1"
 
-ALLOWED_HOSTS = [h for h in get_env('ALLOWED_HOSTS','*').split(',') if h]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
+
+
+# -------------------------------------------
+# APPS
+# -------------------------------------------
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -18,72 +26,104 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
+    'autorizaciones',
     'core',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'autorizaciones.urls'
+
 TEMPLATES = [
     {
-        'BACKEND':'django.template.backends.django.DjangoTemplates',
-        'DIRS':[BASE_DIR/'templates'],
-        'APP_DIRS':True,
-        'OPTIONS':{'context_processors':[
-            'django.template.context_processors.debug',
-            'django.template.context_processors.request',
-            'django.contrib.auth.context_processors.auth',
-            'django.contrib.messages.context_processors.messages',
-        ]}
-    }
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
+
 WSGI_APPLICATION = 'autorizaciones.wsgi.application'
 
+
+# -------------------------------------------
+# DATABASE
+# -------------------------------------------
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': get_env('DB_NAME','autorizaciones'),
-        'USER': get_env('DB_USER','postgres'),
-        'PASSWORD': get_env('DB_PASSWORD',''),
-        'HOST': get_env('DB_HOST','localhost'),
-        'PORT': get_env('DB_PORT','5432'),
+        'NAME': os.getenv('DB_NAME', 'defaultdb'),
+        'USER': os.getenv('DB_USER', 'doadmin'),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', '25060'),
     }
 }
 
+# SSL para PostgreSQL en DO
+sslmode = os.getenv('DJANGO_DB_SSLMODE', 'require')
+if sslmode:
+    DATABASES['default'].setdefault('OPTIONS', {})
+    DATABASES['default']['OPTIONS']['sslmode'] = sslmode
+
+
+# -------------------------------------------
+# PASSWORD VALIDATION
+# -------------------------------------------
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME':'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME':'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME':'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME':'django.contrib.auth.password_validation.NumericPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
+
+# -------------------------------------------
+# INTERNATIONALIZATION
+# -------------------------------------------
 LANGUAGE_CODE = 'es-ar'
 TIME_ZONE = 'America/Argentina/Buenos_Aires'
 USE_I18N = True
 USE_TZ = True
 
+
+# -------------------------------------------
+# STATIC & MEDIA FILES
+# -------------------------------------------
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+
+# -------------------------------------------
+# DEFAULTS
+# -------------------------------------------
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True if DEBUG else False
-CSRF_TRUSTED_ORIGINS = [o for o in get_env('CSRF_TRUSTED_ORIGINS','').split(',') if o]
 
-LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/accounts/login/'
+# -------------------------------------------
+# LOGGING
+# -------------------------------------------
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {'console': {'class': 'logging.StreamHandler'}},
+    'root': {'handlers': ['console'], 'level': 'INFO'},
+}
